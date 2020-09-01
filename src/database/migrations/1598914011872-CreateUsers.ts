8e2f60692e7e0ef2fa2fe2b71d1e019fd4bc665e
Luis Felipe <felipedelta0@gmail.com>
2020-09-01 20:43:59 -0300

    relacionamento nos models

--- src/database/migrations/1598914011872-CreateUsers.ts
+++ src/database/migrations/1598914011872-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1598914011872 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
